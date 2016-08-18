c3723df564858085c15d405e5d71e95c203e9362
Эрик Вильданов <erik_bot@icloud.com>
2016-08-18 22:23:05 +0600

    create tableView in ViewController and change ViewController -> Navigation controller

--- LGS/ViewController.swift
+++ LGS/ViewController.swift
@@ -10,14 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView = UITableView()
+    let tableData = TableDataSource()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 44
+        tableView.registerClass(TableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.dataSource = tableData
+        self.view.addSubview(tableView)
+        setupConstraints()
+        createEditButton()
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func setupConstraints() {
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        let viewsDict = ["tableView" : tableView]
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("H:|[tableView]|", options: [], metrics: nil, views: viewsDict))
+        view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|[tableView]|", options: [], metrics: nil, views: viewsDict))
+    }
+    
+    func createEditButton() {
+        let editButton = UIBarButtonItem(barButtonSystemItem: .Edit, target: self, action: #selector(ViewController.openEdit))
+        
+        self.navigationItem.leftBarButtonItem = editButton
+        
+        let addButton = UIBarButtonItem(image: UIImage(named: "Plus"), style: .Plain, target: self, action: #selector(ViewController.addNewItem))
+        self.navigationItem.rightBarButtonItem = addButton
+    }
+    
+    var flag = true
+    
+    func openEdit(){
+        if flag {
+            tableView.editing = true
+            flag = false
+        } else {
+            tableView.editing = false
+            flag = true
+        }
+    }
+    
+    func addNewItem(){
+        let addFormViewController = AddFormViewController()
+        self.navigationController?.pushViewController(addFormViewController, animated: true)
     }
 
 
